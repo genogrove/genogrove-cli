4aabc64a48a2790f4da01f02a853cc9f39adaf81
riasc <richard.schaefer@zoho.com>
2025-10-14 23:57:26 -0500

    added bed reader class

--- include/bed_reader.hpp
+++ include/bed_reader.hpp
@@ -17,6 +17,37 @@
 #include <algorithm>
 
 // genogrove
-#include <
+#include <file_reader.hpp>
+#include <genogrove/data_type/interval.hpp>
+
+// zlib
+#include <zlib.h>
+#include <sstream>
+
+// htslib
+#include <htslib/hts.h>
+#include <htslib/bgzf.h>
+#include <htslib/kseq.h>
+#include <htslib/kstring.h>
+
+namespace gdt = genogrove::data_type;
+
+class bed_reader : public file_reader {
+public:
+    bed_reader(const std::filesystem::path&, bool gzipped);
+    bool read_next(file_entry& entry) override;
+    bool has_next() override;
+    std::string get_error_message() override;
+    size_t getCurrentLine() override;
+    ~bed_reader();
+
+private:
+    BGZF* bgzfFile;
+    size_t line_num;
+    std::string error_message;
+    //        std::unique_ptr<std::ifstream> inputStream;
+    //        size_t lineNum;
+}
+
 
 #endif //GENOGROVE_CLI_BEDREADER_HPP
