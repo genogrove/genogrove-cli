86bcdc91a978908066fbf5adb97b116e54e96188
riasc <richard.schaefer@zoho.com>
2025-10-16 23:39:04 -0500

    index subcall

---
+++ include/subcall/index.hpp
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Indentifier: MIT
+ *
+ * Copyright (c) 2025 Richard A. Schäfer
+ *
+ * This file is part of genogrove and is licensed under the terms of the MIT license.
+ * See the LICENSE file in the root of the repository for more information.
+ */
+#ifndef GENOGROVE_CLI_INDEX_HPP
+#define GENOGROVE_CLI_INDEX_HPP
+
+// standard
+#include <cstdlib>
+
+// genogrove
+
+
+#endif //GENOGROVE_CLI_INDEX_HPP
